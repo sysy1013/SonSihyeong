91aefbf507f319af80fb93758b478e92317c7ede
SiHyeong <tlgud1013@gmail.com>
2021-07-07 02:31:58 +0900

    step 4

--- python_problem_76/python_problem/python_problem.py
+++ python_problem_76/python_problem/python_problem.py
@@ -1,12 +1,16 @@
 num = 0
 
 while True:
-    try:
-        num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-        if num >= 4:
-            raise TypeError
-        break
-    except ValueError:
-        print("정수를 입력하세요.")
-    except TypeError:
-        print("1,2,3만 입력해주세요.")
+    while True:
+        try:
+            num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+            if num >= 4:
+                raise TypeError
+            break
+        except ValueError:
+            print("정수를 입력하세요.")
+        except TypeError:
+            print("1,2,3만 입력해주세요.")
+
+    for i in range(0, num):
+        print(f'player A : {i+1}')
