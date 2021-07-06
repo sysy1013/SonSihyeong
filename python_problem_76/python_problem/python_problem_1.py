0ffa55638b03dbb521b827a83b0b40393da3ecab
SiHyeong <tlgud1013@gmail.com>
2021-07-07 02:46:23 +0900

    step 7

--- python_problem_76/python_problem/python_problem_1.py
+++ python_problem_76/python_problem/python_problem_1.py
@@ -1,8 +1,10 @@
+import random
+
 num = 0
 A_count = 0
+b_count = 0
 temp = 0
 total = 0
-b_count = 0
 
 while True:
     while True:
@@ -28,26 +30,18 @@ while True:
         print(f'player A : {i+1}')
     temp = A_count
 
-    while True:
-        try:
-            num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-            if num >= 4:
-                raise TypeError
-            break
-        except ValueError:
-            print("정수를 입력하세요.")
-        except TypeError:
-            print("1,2,3만 입력해주세요.")
+    num = 0
+    num = random.randint(1, 3)
 
     b_count = A_count+num
     total += num
 
     if total > 31:
         for i in range(temp, 32):
-            print(f'playerB : {i+1}')
+            print(f'computer : {i+1}')
         print("playerA win!")
         break
 
     for i in range(temp, b_count):
-        print(f'playerB: {i+1}')
+        print(f'computer: {i+1}')
     temp = b_count
