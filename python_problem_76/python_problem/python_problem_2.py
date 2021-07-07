f9a95fe65debc2c1fe89f66bb6047c9595f3f892
SiHyeong <tlgud1013@gmail.com>
2021-07-07 21:04:00 +0900

    python_problem_2

--- python_problem_76/python_problem/python_problem_2.py
+++ python_problem_76/python_problem/python_problem_2.py
@@ -2,14 +2,56 @@
 # 함수 이름은 변경 가능합니다.
 
 # menu 1
+from typing import Type
+
+
 student_list = []
+
+
+def findStudentByName(studentName: str) -> bool:
+    for student in student_list:
+        if student[0] == studentName:
+            return False
+    return True
 
 
 def Menu1(student):
     student[1] = int(student[1])
     student[2] = int(student[2])
     student_list.append(student)
-    print(student_list)
+
+# menu 2
+
+
+def Menu2():
+    # 학점 부여 하는 코딩
+    for i in range(len(student_list)):
+        if len(student_list) < 4:
+            avg = (student_list[i][1]+student_list[i][2])/2
+            if 100 >= avg >= 90:
+                student_list[i].append("A")
+            elif avg >= 80:
+                student_list[i].append("B")
+            elif avg >= 70:
+                student_list[i].append("C")
+            else:
+                student_list[i].append("D")
+
+
+def Menu3():
+    # 출력 코딩
+    print('-------------------------')
+    print('name\tmid\tfinal\tgrade')
+    print('-------------------------')
+    for i in range(len(student_list)):
+        print(
+            f'{student_list[i][0]}\t{student_list[i][1]}\t {student_list[i][2]}\t{student_list[i][3]}')
+
+
+def Menu4(del_s):
+    # 학생 정보 삭제하는 코딩
+    del student_list[del_s]
+    print(f'{del_s} student information is deleted.')
 
 
 # 학생 정보를 저장할 변수 초기화
@@ -45,3 +87,63 @@ while True:
             print("Score is no positive integer!")
         except NameError:
             print("Already exist name!")
+
+    elif choice == "2":
+        # 예외사항 처리(저장된 학생 정보의 유무)
+        # 예외사항이 아닌 경우 2번 함수 호출
+        # "Grading to all students." 출력
+        try:
+            Menu2()
+
+            if len(student_list) > 4:
+                raise IndexError
+            if len(student_list) == 0:
+                raise ValueError
+        except IndexError:
+            print("Grading to all students.")
+        except ValueError:
+            print("No student Data!")
+
+    elif choice == "3":
+        # 예외사항 처리(저장된 학생 정보의 유무, 저장되어 있는 학생들의 학점이 모두 부여되어 있는지)
+        # 예외사항이 아닌 경우 3번 함수 호출
+        try:
+            Menu3()
+
+            if len(student_list) == 0:
+                ValueError
+            if len(student_list) < 4:
+                IndexError
+
+        except ValueError:
+            print("No student Data!")
+        except IndexError:
+            print("There is a student who did't get grade")
+
+    elif choice == "4":
+        # 예외사항 처리(저장된 학생 정보의 유무)
+        # 예외사항이 아닌 경우, 삭제할 학생 이름 입력 받기
+        # 입력 받은 학생의 존재 유무 체크 후, 없으면 "Not exist name!" 출력
+        # 있으면(예를 들어 kim 이라 하면), 4번 함수 호출 후에 "kim student information is
+        try:
+            # Menu4()
+            del_student = input("Enter the name to delete ")
+
+            if findStudentByName(del_student):
+                raise TypeError
+
+            if len(student_list) == 0:
+                raise IndexError
+
+        except TypeError:
+            print("Not exist name")
+        except ValueError:
+            print("No student Data!")
+    elif choice == "5":
+        # 프로그램 종료 메세지 출력
+        # 반복문 종료
+        print("Exit Program!")
+        exit()
+
+    else:
+        print("Wrong number. Choose again")
